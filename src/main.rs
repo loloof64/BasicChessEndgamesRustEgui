25d4ed78bc463797b2ec2dc96f7bf47c472e41be
loloof64 <laurent.bernabe@gmail.com>
2022-08-30 10:20:46 +0200

    Simple Egui experiment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Basic chess endgames",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+
+struct MyApp {
+    name: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self { 
+            name: "Your name".to_string(),
+         }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical(|ui| {
+                ui.text_edit_singleline(&mut self.name);
+                ui.label(format!("Hello, {} !", self.name));
+            });
+        });
+    }
 }
